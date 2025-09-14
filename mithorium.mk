c994e60c5f181f6824b13284ce8971f5ea7f734d
0xKANARIA <enashinonome264@gmail.com>
2025-09-14 05:29:19 +0000

    Revert "mithorium-common: Migrate to AIDL camera provider"
    This reverts commit f6bc020bd77c1d6427a65ce8db8c96d9aca2fe08.

--- mithorium.mk
+++ mithorium.mk
@@ -190,7 +190,8 @@ endif
 ifneq ($(TARGET_USES_DEVICE_SPECIFIC_CAMERA_PROVIDER),true)
 MITHORIUM_PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.camera.provider-service_32.lineage
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 endif
 
 MITHORIUM_PRODUCT_PACKAGES += \
