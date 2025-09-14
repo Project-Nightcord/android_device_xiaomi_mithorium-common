56876654f4aff8227c89c95f7d40a832b4b7c61f
0xKANARIA <enashinonome264@gmail.com>
2025-09-14 05:15:10 +0000

    Revert "revert: mithorium-common: Mark CNE symlink target as required package"
    This reverts commit ee657c282acd0191971289b9d1ae547c3e357916.

--- Android.mk
+++ Android.mk
@@ -12,16 +12,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-CNE_LIBS := libvndfwk_detect_jni.qti.so
-CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/app/CneApp/lib/arm64/,$(notdir $(CNE_LIBS)))
-$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "CneApp lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := mithorium_product_packages
 LOCAL_MODULE_CLASS := FAKE
