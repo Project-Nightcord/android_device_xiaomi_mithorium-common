e64ed77159dc7e031827f86918b83f3794dc6f98
Aqur3nz <enashinonome264@gmail.com>
2026-04-12 07:06:15 +0000

    mithorium-common: Bump Keymaster to V4.0
    V4.0 will allow for "StrongBox" implementations.
    
    ASN.1-encoded key format stores encryption keys securely into the keystore.

--- mithorium.mk
+++ mithorium.mk
@@ -357,7 +357,10 @@ ifneq ($(TARGET_USES_DEVICE_SPECIFIC_KEYMASTER),true)
 MITHORIUM_PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0.vendor
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0-service \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor
 endif
 
 # Lights
